369e4b2a434f2dd6499024bd0b6ff98073212cb3
arttu <ttu91@hotmail.com>
2024-02-10 15:39:26 +0200

    clean unused variables

--- src/TableTable/index.tsx
+++ src/TableTable/index.tsx
@@ -1,4 +1,3 @@
-
 type Title = string | JSX.Element;
 
 type ColumnDefinition = {
@@ -22,18 +21,12 @@ export const TableTable = ({
         gridTemplateColumns: columns.map((_) => "200px").join(" "),
       }}
     >
-      {columns.map((col, index) => (
-        <div key={col.key}>
-          {col.title}
-        </div>
+      {columns.map((col) => (
+        <div key={col.key}>{col.title}</div>
       ))}
       {data.map((row, rowIndex) =>
-        columns.map((col, colIndex) => (
-          <div
-            key={`${col.title}-${rowIndex}`}
-          >
-            {col.getValue(row)}
-          </div>
+        columns.map((col) => (
+          <div key={`${col.title}-${rowIndex}`}>{col.getValue(row)}</div>
         ))
       )}
     </div>
