2d8b1e2c54758bbed8eeccb61ca82115ddd15b96
arttu <ttu91@hotmail.com>
2024-02-10 19:14:48 +0200

    add pre-commit hook

--- src/App.tsx
+++ src/App.tsx
@@ -5,31 +5,42 @@ import { data, Todo } from './testData';
 const columns: Array<ColumnDefinition<Todo>> = [
   {
     title: 'Id',
-    getValue: todo => todo.id,
+    getValue: (todo) => todo.id,
     key: 'id',
   },
   {
     title: 'User Id',
-    getValue: todo => todo.userId,
+    getValue: (todo) => todo.userId,
     key: 'user-id',
   },
   {
     title: 'Name',
-    getValue: todo => todo.title,
-    getSortValue: todo => todo.title.length,
+    getValue: (todo) => todo.title,
+    getSortValue: (todo) => todo.title.length,
     key: 'name',
   },
   {
     title: 'Completed',
-    getValue: todo => todo.completed.toString(),
+    getValue: (todo) => todo.completed.toString(),
     key: 'completed',
   },
 ];
 
 const App = () => {
   return (
-    <div style={{ padding: '20px', maxWidth: '1400px', margin: 'auto', backgroundColor: 'lightseagreen' }}>
-      <TableTable tableStyle={{ maxWidth: '1000px' }} columns={columns} data={data} />
+    <div
+      style={{
+        padding: '20px',
+        maxWidth: '1400px',
+        margin: 'auto',
+        backgroundColor: 'lightseagreen',
+      }}
+    >
+      <TableTable
+        tableStyle={{ maxWidth: '1000px' }}
+        columns={columns}
+        data={data}
+      />
     </div>
   );
 };
