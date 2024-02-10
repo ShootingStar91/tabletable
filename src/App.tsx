a6ee8f91971edebc05d18c262a68b6e9ad2afe7a
arttu <ttu91@hotmail.com>
2024-02-10 16:49:48 +0200

    Now acually sortable table. Also some style fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
 import { TableTable } from "./TableTable";
+import { ColumnDefinition } from "./TableTable";
 
-const columns = [
-  { title: <span style={{ color: 'green', backgroundColor: 'lightblue' }}>Points</span>, getValue: (val: number) => val, key: "points" },
-  { title: "time", getValue: (val: number) => val, key: "time" },
+const columns: Array<ColumnDefinition<DataValue>> = [
+  {
+    title: (
+      <span
+        style={{
+          color: "green",
+          backgroundColor: "lightblue",
+          overflow: "scroll",
+          maxWidth: "100px",
+        }}
+      >
+        Points Points
+      </span>
+    ),
+    getValue: (val: DataValue) => val.points,
+    key: "points",
+  },
+  { title: "time", getValue: (val: DataValue) => val.time, key: "time" },
 ];
 
-const data = [1, 2, 3, 4, 5];
+type DataValue = { points: string; time: string };
+
+const data: Array<DataValue> = [5, 3, 3, 4, 0, 6, 2, 8].map((num) => ({
+  points: (num * 2).toString().padStart(5, "0"),
+  time: (num * 100).toString().padStart(5, "0"),
+}));
 
 const App = () => {
   return (
--- src/TableTable/index.tsx
+++ src/TableTable/index.tsx
@@ -1,34 +1,78 @@
-type Title = string | JSX.Element;
+import { useState } from "react";
+import { orderBy } from "lodash";
 
-type ColumnDefinition = {
-  title: Title;
-  getValue: (dataVal: number) => number;
+type CellContent = string | JSX.Element | number;
+
+export type ColumnDefinition<T> = {
+  title: CellContent;
+  getValue: (dataVal: T) => CellContent;
   key: string;
 };
 
-export const TableTable = ({
+export const TableTable = <T,>({
   columns,
   data,
 }: {
-  columns: Array<ColumnDefinition>;
-  data: Array<number>;
+  columns: Array<ColumnDefinition<T>>;
+  data: Array<T>;
 }) => {
+  const [sortMode, setSortMode] = useState<{
+    col: ColumnDefinition<T>;
+    asc: boolean;
+  } | null>(null);
+  const sortBy = (col: ColumnDefinition<T>) => {
+    if (!sortMode) {
+      setSortMode({ col, asc: true });
+      return;
+    }
+    if (sortMode.col === col) {
+      if (sortMode.asc) {
+        setSortMode({ col, asc: false });
+        return;
+      }
+      setSortMode(null);
+      return;
+    }
+    setSortMode({ col, asc: true });
+  };
+  const getSortIcon = (col: ColumnDefinition<T>) => {
+    if (col === sortMode?.col) {
+      return sortMode?.asc ? "[ASC]" : "[DESC]";
+    }
+    return "";
+  };
+  const sortedData = sortMode
+    ? orderBy(
+        data,
+        (item) => sortMode.col.getValue(item),
+        sortMode?.asc ? "asc" : "desc"
+      )
+    : data;
+  const dataCells = sortedData.flatMap((row) =>
+    columns.map((col) => col.getValue(row))
+  );
+  console.log({ dataCells, data });
   return (
     <div
       id="tabletable"
       style={{
         display: "grid",
-        gridTemplateColumns: columns.map((_) => "200px").join(" "),
+        gridTemplateColumns: columns.map((_) => "1fr").join(" "),
+        backgroundColor: "lightgray",
       }}
     >
       {columns.map((col) => (
-        <div key={col.key}>{col.title}</div>
+        <div
+          style={{ userSelect: "none" }}
+          key={col.key}
+          onClick={() => sortBy(col)}
+        >
+          {col.title} {getSortIcon(col)}
+        </div>
       ))}
-      {data.map((row, rowIndex) =>
-        columns.map((col) => (
-          <div key={`${col.title}-${rowIndex}`}>{col.getValue(row)}</div>
-        ))
-      )}
+      {dataCells.map((row) => (
+        <div>{row}</div>
+      ))}
     </div>
   );
 };
